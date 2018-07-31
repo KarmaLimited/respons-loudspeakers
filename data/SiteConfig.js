eedbb0974e5cda08df4bfd65881c6e7ef814b0d8
Stefan Lachmann <stefanlachmann@hotmail.com>
2018-07-31 14:26:37 +0200

    added date

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,9 +1,13 @@
 module.exports = {
-  corporationName: '',
-  siteTitle: '',
+  corporationName: 'Respons Loudspeakers',
+  siteTitle: 'Respons loudspeakers',
   siteDescription: '',
   siteKeywords: 'SPA',
   siteUrl: 'https://respons-loudspeakers.netlify.com/',
   siteLogo: '../src/icons/logo.ico',
-  // googleAnalyticsID: '',
+  copyright: `Copyright © Respons Loudspeakers ${new Date().getFullYear()}. All Rights Reserved.`, // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff', // Used for setting manifest background color.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteRss: '/rss.xml',
 };
