21d660f3a053fc498272acfb20126fc7eb1c0f47
Stefan Lachmann <stefanlachmann@hotmail.com>
2018-08-02 16:07:03 +0200

    fix build err

--- src/sections/TopSection.jsx
+++ src/sections/TopSection.jsx
@@ -6,20 +6,20 @@ import logoWhiteGrey from '../icons/logo-grey-and-white.png';
 import topImageSmall from '../images/violinist.png';
 import topImageLarge from '../images/arindam-mahanta-217437-unsplash.jpg';
 
-const width = document.documentElement.clientWidth || document.body.clientWidth;
-
-let coverImage;
-let logoSvg;
-
-if (width <= 420) {
-  coverImage = topImageSmall;
-  logoSvg = logoWhiteGrey;
-} else {
-  coverImage = topImageLarge;
-  logoSvg = logoWhite;
-}
-
 const TopSection = () => {
+  const width =
+    document.documentElement.clientWidth || document.body.clientWidth;
+
+  let coverImage;
+  let logoSvg;
+
+  if (width <= 420) {
+    coverImage = topImageSmall;
+    logoSvg = logoWhiteGrey;
+  } else {
+    coverImage = topImageLarge;
+    logoSvg = logoWhite;
+  }
   const styles = {
     container: {
       alignItems: 'center',
