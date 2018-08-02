ab18c2098c23f5a7dc55cde251353bfa18f425c7
Stefan Lachmann <stefanlachmann@hotmail.com>
2018-08-02 20:33:24 +0200

    new placeholderimg

--- src/sections/TopSection.jsx
+++ src/sections/TopSection.jsx
@@ -5,7 +5,7 @@ import logoWhite from '../icons/logo-white.svg';
 import logoWhiteGrey from '../icons/logo-grey-and-white.png';
 import topImageSmall from '../images/optimized/violinist.png';
 import topImageLarge from '../images/arindam-mahanta-217437-unsplash.jpg';
-import placeHolderImage from '../images/optimized/placeholderImg.jpg';
+import placeHolderImage from '../images/optimized/placeholderImg.png';
 
 const TopSection = () => {
   let coverImage;
